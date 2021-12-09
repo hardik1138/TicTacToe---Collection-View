283a3350c2c6529356caa1acf5560f5d1beee07f
Hardik <31307659+hardik1138@users.noreply.github.com>
2021-12-09 11:27:03 +0530

    Add files via upload

---
+++ Assignment8/Assignment8Tests/Assignment8Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Assignment8Tests.swift
+//  Assignment8Tests
+//
+//  Created by DCS on 08/12/21.
+//  Copyright © 2021 DCS. All rights reserved.
+//
+
+import XCTest
+@testable import Assignment8
+
+class Assignment8Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
